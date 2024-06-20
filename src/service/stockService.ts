37e281231640a553e65ec9277264b38cc7a10591
Guilherme Souza <gui.souzamatos@gmail.com>
2024-06-20 05:32:53 -0300

    Ajuste de endpoint de controle de Estoque
    Ajuste de endpoint de controle de Estoque

--- src/service/stockService.ts
+++ src/service/stockService.ts
@@ -3,6 +3,8 @@ import {StockRequestBody} from "../models/interface/stock-request-body";
 import ProductService from "./productService";
 import {Stock} from "../models/entity/stock";
 import StockRepository from "../repository/stockRepository";
+import {StockObject} from "../models/interface/stock-object";
+import {Product} from "../models/entity/product";
 
 export default class StockService {
     private repository: StockRepository = new StockRepository();
@@ -17,12 +19,7 @@ export default class StockService {
                 return;
             }
             if (!newStock) {
-                newStock = new Stock();
-                newStock.quantity = stock.quantity;
-                newStock.addition = body.add;
-                newStock.product = product;
-                newStock.totalValue = product.price * stock.quantity;
-                return await newStock.save();
+                return await this.createNewStock(body, stock, product);
             }
             if (body.add) {
                 newStock.quantity += stock.quantity;
@@ -35,6 +32,18 @@ export default class StockService {
             newStock.totalValue = product.price * stock.quantity;
             return await newStock.save();
         }
+    }
+
+    createNewStock = async (body: StockRequestBody, stock: StockObject, product: Product) => {
+        const newStock = new Stock();
+        newStock.quantity = stock.quantity;
+        if (!body.add) {
+            throw { status: 400, message: 'Estoque inválido.' };
+        }
+        newStock.addition = true;
+        newStock.product = product;
+        newStock.totalValue = product.price * stock.quantity;
+        return await newStock.save();
     }
 
     findStockByProduct = async (productId: number): Promise<Stock | null> => {
